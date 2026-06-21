fd72ce4fc7a1eb2677baec0931d7f73339488274
Preslava-7 <presi_biserova@abv.bg>
2026-06-21 21:42:09 +0300

    feat: implement core business services and application logic

---
+++ src/main/java/bg/softuni/eternalbouquet/service/orderItem/OrderItemService.java
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+package bg.softuni.eternalbouquet.service.orderItem;
+
+import bg.softuni.eternalbouquet.mapper.orderItem.OrderItemMapper;
+import bg.softuni.eternalbouquet.model.dto.orderItem.OrderItemDto;
+import bg.softuni.eternalbouquet.model.entity.bouquet.Bouquet;
+import bg.softuni.eternalbouquet.model.entity.order.Order;
+import bg.softuni.eternalbouquet.model.entity.orderItem.OrderItem;
+import bg.softuni.eternalbouquet.repository.bouquet.BouquetRepository;
+import bg.softuni.eternalbouquet.repository.order.OrderRepository;
+import bg.softuni.eternalbouquet.repository.orderItem.OrderItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderItemService {
+
+=======
+package bg.softuni.eternalbouquet.service.orderItem;public class OrderItemService {
+>>>>>>> 6069ab8 (feat: add domain entities repositories and service layer)
+}
